850417900adb5a1d9fbe8f0f6c2b65d66a95a217
Adam Scott <adam.scott@cfpb.gov>
2015-12-03 10:42:38 -0500

    test scholarship grant and savings

---
+++ test/scholarship-spec.js
@@ -0,0 +1,65 @@
+var scholarship = require( '../src/scholarship' );
+var data = require( '../src/default-values' );
+
+var chai = require('chai');
+var expect = chai.expect;
+
+
+describe( 'set scholarship and grant values', function() {
+  it( 'correctly sets the pell max when year one costs are less than the maximum pell grant', function() {
+    data.undergrad = true;
+    data.yearOneCosts = 5000;
+    scholarship( data );
+    expect( data.pellMax ).to.equal( 5000 );
+  });
+
+  it( 'correctly sets the pell max when year one costs are more than the maximum pell grant', function() {
+    data.undergrad = true;
+    data.yearOneCosts = 10000;
+    scholarship( data );
+    expect( data.pellMax ).to.equal( data.pellCap );
+  });
+
+  it( 'correctly sets the pell max for non undergrad programs', function() {
+    data.undergrad = false;
+    data.yearOneCosts = 10000;
+    scholarship( data );
+    expect( data.pellMax ).to.equal( 0 );
+  });
+
+  it( 'calculates the total grants', function() {
+    data.undergrad = true;
+    data.yearOneCosts = 10000;
+    data.pell = 4400;
+    data.scholarships = 2000;
+    scholarship( data );
+    expect( data.grantsTotal ).to.equal( 6400 );
+  });
+
+  it( 'calculates the first year net cost', function() {
+    data.yearOneCosts = 10000;
+    data.pell = 4400;
+    data.scholarships = 2000;
+    scholarship( data );
+    expect( data.firstYearNetCost ).to.equal( 3600 );
+  });
+
+  it( 'defaults to 0 total contributions', function() {
+    scholarship( data );
+    expect( data.savingsTotal ).to.equal( 0 );
+  });
+
+  it( 'calculates total contributions', function() {
+    data.undergrad = true;
+    data.savings = 10000;
+    data.family = 4400;
+    data.state529plan = 2000;
+    data.workstudy = 2000;
+    scholarship( data );
+    expect( data.savingsTotal ).to.equal( 18400 );
+  });
+
+  it( 'calculates total grants and savings', function() {
+    expect( data.totalgrantsandsavings ).to.equal( 24800 );
+  });
+});
