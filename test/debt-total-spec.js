cdaf2417e93a7a406720ff5b9c85e4956691c599
Chuck Werner <mistergone@gmail.com>
2016-01-13 13:00:57 -0500

    deferPeriod added to privateLoanMulti, README updated

--- test/debt-total-spec.js
+++ test/debt-total-spec.js
@@ -16,9 +16,9 @@ describe( 'calculates debt totals', function() {
 
   it ( 'handles multiple private loans', function() {
     data.privateLoanMulti = [
-        { 'amount': 2000, 'rate': .079 },
-        { 'amount': 3000, 'rate': .061 },
-        { 'amount': 4000, 'rate': .041 }
+        { 'amount': 2000, 'rate': .079, 'deferPeriod': 6 },
+        { 'amount': 3000, 'rate': .061, 'deferPeriod': 6 },
+        { 'amount': 4000, 'rate': .041, 'deferPeriod': 6 }
       ];
     debtTotal( data );
     expect( data.privateLoanTotal ).to.equal( 9896 + 14196 + 17968 );
